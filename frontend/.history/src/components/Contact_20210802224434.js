a841594895fd928038c95bf04ce6e7d0961940c4
Eidmone <eidmonetagaca@gmail.com>
2021-08-02 22:58:27 -0700

    save point

--- frontend/.history/src/components/Contact_20210802224434.js
+++ frontend/.history/src/components/Contact_20210802224434.js
@@ -8,7 +8,6 @@ const Contact = () => {
         <h3>Contact Me</h3>
         <p className="text-base font-thin">I'd love to hear your thoughts!</p>
       </header>
-
       <div className="md:mx-6 flex flex-col flex-wrap md:flex-row justify-between">
         <div className="md:w-6/12 md:px-0 p-5 my-5">
           <div>
@@ -65,47 +64,45 @@ const Contact = () => {
         </div>
 
         <div className="bg-gray px-5 py-10 md:py-8 sm:p-8 my-2 md:rounded-lg shadow-lg  justify-between w-full  md:w-6/12 ">
-          <div className="form">
-            <form
-              className="flex flex-col space-y-3 m-auto w-full"
-              name="contact"
-              method="post"
-            >
-              <input type="hidden" name="form-name" value="contact" />
+          <form
+            className="flex flex-col space-y-3 m-auto w-full"
+            name="contact"
+            method="post"
+          >
+            <input type="hidden" name="form-name" value="contact" />
 
-              <label htmlFor="name">Name</label>
-              <input
-                type="text"
-                name="name"
-                id="name"
-                className="gradient"
-                required
-              ></input>
-              <label htmlFor="email">Email</label>
-              <input
-                type="email"
-                name="email"
-                id="email"
-                className="gradient"
-                required
-              ></input>
-              <label htmlFor="message">Message</label>
-              <textarea
-                name="message"
-                id="message"
-                cols="25"
-                rows="5"
-                className="gradient"
-                required
-              ></textarea>
-              <button
-                type="submit"
-                className="border border-gray-500 p-2 rounded-lg w-auto mr-auto shadow-md"
-              >
-                Send Message
-              </button>
-            </form>
-          </div>
+            <label htmlFor="name">Name</label>
+            <input
+              type="text"
+              name="name"
+              id="name"
+              className="gradient"
+              required
+            ></input>
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              className="gradient"
+              required
+            ></input>
+            <label htmlFor="message">Message</label>
+            <textarea
+              name="message"
+              id="message"
+              cols="25"
+              rows="5"
+              className="gradient"
+              required
+            ></textarea>
+            <button
+              type="submit"
+              className="border border-gray-500 p-2 rounded-lg w-auto mr-auto shadow-md"
+            >
+              Send Message
+            </button>
+          </form>
         </div>
       </div>
       <div
--- frontend/.history/src/components/Contact_20210802225130.js
+++ frontend/.history/src/components/Contact_20210802225130.js
@@ -65,47 +65,45 @@ const Contact = () => {
         </div>
 
         <div className="bg-gray px-5 py-10 md:py-8 sm:p-8 my-2 md:rounded-lg shadow-lg  justify-between w-full  md:w-6/12 ">
-          <div className="form">
-            <form
-              className="flex flex-col space-y-3 m-auto w-full"
-              name="contact"
-              method="post"
-            >
-              <input type="hidden" name="form-name" value="contact" />
+          <form
+            className="flex flex-col space-y-3 m-auto w-full"
+            name="contact"
+            method="post"
+          >
+            <input type="hidden" name="form-name" value="contact" />
 
-              <label htmlFor="name">Name</label>
-              <input
-                type="text"
-                name="name"
-                id="name"
-                className="gradient"
-                required
-              ></input>
-              <label htmlFor="email">Email</label>
-              <input
-                type="email"
-                name="email"
-                id="email"
-                className="gradient"
-                required
-              ></input>
-              <label htmlFor="message">Message</label>
-              <textarea
-                name="message"
-                id="message"
-                cols="25"
-                rows="5"
-                className="gradient"
-                required
-              ></textarea>
-              <button
-                type="submit"
-                className="border border-gray-500 p-2 rounded-lg w-auto mr-auto shadow-md"
-              >
-                Send Message
-              </button>
-            </form>
-          </div>
+            <label htmlFor="name">Name</label>
+            <input
+              type="text"
+              name="name"
+              id="name"
+              className="gradient"
+              required
+            ></input>
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              className="gradient"
+              required
+            ></input>
+            <label htmlFor="message">Message</label>
+            <textarea
+              name="message"
+              id="message"
+              cols="25"
+              rows="5"
+              className="gradient"
+              required
+            ></textarea>
+            <button
+              type="submit"
+              className="border border-gray-500 p-2 rounded-lg w-auto mr-auto shadow-md"
+            >
+              Send Message
+            </button>
+          </form>
         </div>
       </div>
       <div
